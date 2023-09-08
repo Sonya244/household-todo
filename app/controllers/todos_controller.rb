391d8a4ad4da17de46e2706062f272c85b9a0f7c
Boris Beginin <gen3212@gmail.com>
2023-09-08 14:46:06 +0200

    basic setup

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,17 @@
+class TodosController < ApplicationController
+  def create
+    todo = Todo.create!(todo_params)
+
+    render json: todo, status: :created
+  end
+
+  def index
+    todos = Todo.all
+
+    render json: todos, status: :ok
+  end
+
+  def todo_params
+    params.permit(:description, :expire_at)
+  end
+end
