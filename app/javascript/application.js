fbe2d5d3f63828e40c5209c649dbac47c3551c94
Sonya244 <115551600+Sonya244@users.noreply.github.com>
2023-09-28 22:09:19 +0200

    basic_backend_integration (#3)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,31 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// API contract
+// expected data format for APi
+// POST localhost:3000/todos 
+//{
+// description: string, required
+// expire_at: string optional
+//}
+
+// takes string from input
+// 
+function createToDo(str) {
+    const obj = { description: str }
+    const json = JSON.stringify(obj)
+
+    fetch('/todos', {
+        method: "POST",
+        body: json
+    })
+}
+// fetch('/todos')
+function onBtnClick() {
+    const input = document.getElementById('input');
+    console.log(input.value);
+    createToDo(input.value)
+}
+document.querySelector('.addBtn').onclick = onBtnClick;
+
