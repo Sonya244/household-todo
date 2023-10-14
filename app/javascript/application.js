5ec47c59591ae8a579837a55efc93552b6af777a
Sofia Petrova <moonjuice1321@gmail.com>
2023-10-14 15:51:41 +0200

    display tasks function fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,34 +1,65 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
 // API contract
 // expected data format for APi
-// POST localhost:3000/todos 
+// POST localhost:3000/todos
 //{
 // description: string, required
 // expire_at: string optional
 //}
 
 // takes string from input
-// 
+//
 function createToDo(str) {
-    const obj = { description: str }
-    const json = JSON.stringify(obj)
+  const obj = { description: str };
+  const json = JSON.stringify(obj);
 
-    fetch('/todos', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: json
-    })
+  return fetch("/todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: json,
+  });
 }
 // fetch('/todos')
-function onBtnClick() {
-    const input = document.getElementById('input');
-    console.log(input.value);
-    createToDo(input.value)
-}
-document.querySelector('.addBtn').onclick = onBtnClick;
 
+document.querySelector(".addBtn").onclick = onBtnClick;
+
+function getTasks() {
+    fetch("/todos").then((resp) => resp.json().then(displayTasks));
+}
+const UnList = document.getElementById("myUl");
+
+function resetList() {
+    UnList.innerHTML = ''
+}
+
+
+getTasks()
+
+function onBtnClick() {
+    const input = document.getElementById("input");
+    console.log(input.value);
+    // this function resolves later then getTasks
+    createToDo(input.value).then(() => {
+        resetList()
+        getTasks()
+    })
+  }
+
+
+
+function displayTasks(tasks) {
+  console.log(tasks);
+
+  tasks.forEach((task) => {
+    const taskLi = document.createElement("li");
+
+    taskLi.textContent = task.description;
+
+    UnList.append(taskLi);
+  });
+}
